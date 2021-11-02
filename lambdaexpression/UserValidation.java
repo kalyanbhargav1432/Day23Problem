63626cfc2ebb50dd6d11686269ce87a9db498cbe
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-11-02 12:37:22 +0530

    Refactored:Functionality to use lambda function

--- lambdaexpression/UserValidation.java
+++ lambdaexpression/UserValidation.java
@@ -6,16 +6,16 @@ interface UserValidation {
 
 	public static boolean validateFirstName(String firstName) {
 		boolean isValid = isValid(firstName, (String info) -> {
-			String regx = "^([A-Z]{1}[a-z]{2,})";
-			return info.matches(regx);
+			String regexFirst = "^([A-Z]{1}[a-z]{2,})";
+			return info.matches(regexFirst);
 		});
 		return isValid;
 	}
 
 	public static boolean validateLastName(String lastName) {
 		boolean isValid = isValid(lastName, (String info) -> {
-			String regxLast = "^([A-Z]{1}[a-z]{2,})";
-			return info.matches(regxLast);
+			String regexLast = "^([A-Z]{1}[a-z]{2,})";
+			return info.matches(regexLast);
 		});
 		return isValid;
 	}
@@ -26,16 +26,16 @@ interface UserValidation {
 
 	public static boolean validateMobile(String mobileNumber) {
 		boolean isValid = isValid(mobileNumber, (String info) -> {
-			String regxMobile = "^(\\d{2})( )([7-9]{1})(\\d{9})";
-			return info.matches(regxMobile);
+			String regexMobileNumber = "^(\\d{2})( )([7-9]{1})(\\d{9})";
+			return info.matches(regexMobileNumber);
 		});
 		return isValid;
 	}
 
 	public static boolean validatePassword(String password) {
 		boolean isValid = isValid(password, (String info) -> {
-			String regxPassword = "^([A-Z]{1}[a-zA-Z]{8,}[@#$%^&+=]{1}[0-9]{3,})";
-			return info.matches(regxPassword);
+			String regexPassword = "^([A-Z]{1}[a-zA-Z]{8,}[@#$%^&+=]{1}[0-9]{3,})";
+			return info.matches(regexPassword);
 		});
 		return isValid;
 	}
@@ -43,8 +43,8 @@ interface UserValidation {
 	public static boolean validateEmails(String email) {
 
 		boolean isValid = isValid(email, (String info) -> {
-			String regxemail = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-			return info.matches(regxemail);
+			String regexemail = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+			return info.matches(regexemail);
 		});
 		return isValid;
 	}
